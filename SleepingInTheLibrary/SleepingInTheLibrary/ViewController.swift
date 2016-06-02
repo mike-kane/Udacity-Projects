f511b68de8e3635e564532d619da7f4c32e0da71
Mike Kane <mike.kane@students.makeschool.com>
2016-06-01 23:38:22 -0700

    initial commit

---
+++ SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  SleepingInTheLibrary
+//
+//  Created by Jarrod Parkes on 11/3/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var grabImageButton: UIButton!
+    
+    // MARK: Actions
+    
+    @IBAction func grabNewImage(sender: AnyObject) {
+        setUIEnabled(false)
+        getImageFromFlickr()
+    }
+    
+    // MARK: Configure UI
+    
+    private func setUIEnabled(enabled: Bool) {
+        photoTitleLabel.enabled = enabled
+        grabImageButton.enabled = enabled
+        
+        if enabled {
+            grabImageButton.alpha = 1.0
+        } else {
+            grabImageButton.alpha = 0.5
+        }
+    }
+    
+    // MARK: Make Network Request
+    
+    private func getImageFromFlickr() {
+        
+       
+        
+        
+        
+    }
+    
+    private func escapedParameters(parameters: [String:AnyObject]) -> String {
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                //make sure it's a string
+                let stringValue = "\(value)"
+                
+                //escape it
+                let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+                
+                //append it
+                keyValuePairs.append(key + "=" + "\(escapedValue)")
+            }
+            return "?\(keyValuePairs.joinWithSeparator("&"))"
+        }
+    }
+}
