4b62d7c4fffed4fbb19c157808da231265c17e84
Mike Kane <mike.kane@students.makeschool.com>
2016-06-04 01:13:07 -0700

    finish HearthStone playground

--- JSON-Parsing-Hearthstone-Init.playground/Contents.swift
+++ JSON-Parsing-Hearthstone-Init.playground/Contents.swift
@@ -22,6 +22,92 @@ var parsedHearthstoneJSON = try! NSJSONSerialization.JSONObjectWithData(rawHeart
 
 func parseJSONAsDictionary(dictionary: NSDictionary) {
     /* Start playing with JSON here... */
-}
+    
+    guard let arrayOfDictionaries = dictionary["Basic"] as? [[String:AnyObject]] else {
+        print("can't find key 'basic'")
+        return
+    }
+    
+        var totalCommonMinions = 0
+        var runningCostOfCommonMinions = 0
+        var minionWithCostOf5 = 0
+        var weaponWithDurabilityOf2 = 0
+        var minionsWithBattleCry = 0
+        var statsToCostRatio = [Double]()
+        var avgStatsToCostRatio: Double = 0.0
+    
+        for cardDictionary in arrayOfDictionaries {
+        
+            guard let cardType = cardDictionary["type"] as? String else {
+                print("can't access 'type' in \(cardDictionary)")
+                return
+            }
+        
+            if cardType == "Minion" {
+                
+                guard let cost = cardDictionary["cost"] as? Int else {
+                    print("cant access cost for card \(cardDictionary)")
+                    return
+                }
+                
+                guard let rarity = cardDictionary["rarity"] as? String else {
+                    print("can't access rarity for card \(cardDictionary)")
+                    return
+                }
+                
+                guard let health = cardDictionary["health"] as? Int else {
+                    print("can't access health for \(cardDictionary)")
+                    return
+                }
+                
+                guard let attack = cardDictionary["attack"] as? Int else {
+                    print("can't access attack for \(cardDictionary)")
+                    return
+                }
+                
+                if let text = cardDictionary["text"] as? String where text.rangeOfString("Battlecry") != nil {
+                    minionsWithBattleCry += 1
+                }
+                
+                if rarity == "Common" {
+                    totalCommonMinions += 1
+                    runningCostOfCommonMinions += cost
+                }
+                
+                if cost == 5 {
+                    minionWithCostOf5 += 1
+                }
+                
+                if cost != 0 {
+                    var ratio: Double = Double(attack + health) / Double(cost)
+                    statsToCostRatio.append(ratio)
+                }
+                
+            }
+            
+            
+            if cardType == "Weapon" {
+                
+                guard let durability = cardDictionary["durability"] as? Int else {
+                    print("weapon \(cardDictionary) has no durability")
+                    return
+                }
+                if durability == 2 {
+                    weaponWithDurabilityOf2 += 1
+                }
+            }
+        }
+    
+        var avgCostOfCommonMinions: Double = Double(runningCostOfCommonMinions) / Double(totalCommonMinions)
+    
+        for avg in statsToCostRatio {
+            avgStatsToCostRatio += avg
+        }
+    
+        avgStatsToCostRatio = avgStatsToCostRatio / Double(statsToCostRatio.count)
+    
+        
+    }
+
 
 parseJSONAsDictionary(parsedHearthstoneJSON)
