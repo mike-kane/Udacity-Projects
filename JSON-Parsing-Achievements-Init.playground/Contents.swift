c03c4c04e3d12db08167356bd57d66f7d96b0c52
Mike Kane <mike.kane@students.makeschool.com>
2016-06-04 09:23:55 -0700

    finish achievements playground

--- JSON-Parsing-Achievements-Init.playground/Contents.swift
+++ JSON-Parsing-Achievements-Init.playground/Contents.swift
@@ -24,6 +24,9 @@ func parseJSONAsDictionary(dictionary: NSDictionary) {
     /* Start playing with JSON here... */
     
     var matchmakingIDs = [Int]()
+    var pointTotal: Double = 0.0
+    var totalMatchmakingAchievements = 0
+    var achievementsGreaterThan10 = 0
     
     guard let categoriesDictionary = dictionary["categories"] as? [NSDictionary] else {
         print("cant access 'categories'")
@@ -34,11 +37,6 @@ func parseJSONAsDictionary(dictionary: NSDictionary) {
         print("cant access achievements")
         return
     }
-    
-    
-    var matchMakingIDs = [Int]()
-    
-    var pointTotal: Double = 0.0
     
     for category in categoriesDictionary {
         
@@ -58,8 +56,55 @@ func parseJSONAsDictionary(dictionary: NSDictionary) {
                 
                 matchmakingIDs.append(id)
             }
+            
         }
     }
+    
+    for achievement in achievementsDictionary {
+        
+        guard let points = achievement["points"] as? Int else {
+            print("can't access points")
+            return
+        }
+        
+        guard let categoryID = achievement["categoryId"] as? Int else {
+            print("can't access categoryId")
+            return
+        }
+        
+        guard let title = achievement["title"] as? String else {
+            print("cant access title")
+            return
+        }
+        
+        guard let description = achievement["description"] as? String else {
+            print("can't access description")
+            return
+        }
+        
+        if title == "Cool Running" {
+            print(description)
+        }
+        
+        if matchmakingIDs.contains(categoryID) {
+            totalMatchmakingAchievements += 1
+        }
+        
+        
+        
+        pointTotal += Double(points)
+        
+        if points > 10 {
+            achievementsGreaterThan10 += 1
+        }
+        
+        
+        
+        
+        
+    }
+    
+    print(pointTotal / 737.0)
     
 }
 
